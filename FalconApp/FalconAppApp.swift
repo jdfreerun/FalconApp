948f868c8281899836009e635c77c4877fa76667
Albert Fayzullin <jdfreerun@gmail.com>
2022-12-17 13:10:47 +0400

    Initial Commit

---
+++ FalconApp/FalconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconAppApp.swift
+//  FalconApp
+//
+//  Created by McBook on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FalconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
